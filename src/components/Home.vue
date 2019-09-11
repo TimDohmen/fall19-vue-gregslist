0f2b5f1b176bfbcd873a7a65c514f54c64e2c8af
Tim Dohmen <dohmentim@yahoo.com>
2019-09-11 15:17:23 -0600

    homes in state

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home"></div>
+</template>
+
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
